bf246d2d3552c92fd0feec4ab1ddd36a74a9094c
Fynn Michell-Auli <f.michell-auli@live.de>
2023-01-27 12:53:25 +0100

    refactoring

--- sudo.zsh.plugin.zsh
+++ sudo.zsh.plugin.zsh
@@ -1,8 +1,7 @@
 # prepend sudo to buffer
-function prepend-sudo { # Insert "sudo " at the beginning of the line
-  if [[ $BUFFER != "sudo "* ]]; then
-    BUFFER="sudo $BUFFER"; CURSOR+=5
-  fi
+function prepend-sudo {
+  [[ $BUFFER = "sudo "* ]] && return
+  BUFFER="sudo $BUFFER"; CURSOR+=5
 }
 zle -N prepend-sudo
 
